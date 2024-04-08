3e7ae669378bbf2fae071c286da07eee423777e4
JustAgust <charles.sparks@atlasschool.com>
2024-04-08 17:09:24 -0500

    coding style issue

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,13 +4,13 @@
  * insertion_sort_list - sorts a doubly linked list of ints in ascending order
  *@list: dbl linked list to be sorted
  *Return: void
-*/
+ */
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *next, *prev;
 
 	if (!list || !*list || !(*list)->next)
-	return;
+		return;
 
 	current = *list;
 	while (current)
@@ -21,12 +21,12 @@ void insertion_sort_list(listint_t **list)
 		{
 			prev->next = current->next;
 			if (current->next)
-			current->next->prev = prev;
+				current->next->prev = prev;
 			current->next = prev;
 			current->prev = prev->prev;
 			prev->prev = current;
 			if (current->prev)
-			current->prev->next = current;
+				current->prev->next = current;
 			else*list = current;
 			prev = current->prev;
 			print_list(*list);
